44d9b6b3745687cdb00cd4e0d3716029f0c55a9e
Fabio Molinaris <fabiomolinaris@outlook.com>
2023-01-12 11:35:52 +0100

    add some

--- gnome3_keybind_backup.sh
+++ gnome3_keybind_backup.sh
@@ -2,10 +2,11 @@
 
 # thanks to https://gist.github.com/peterrus/e59a96688a4d49ee3d9302c0d3ff5fdd#file-gnome3_keybind_backup-sh
 
-mkdir -p gnome3-keybind-backup
+set -e
 
-dconf dump '/org/gnome/desktop/wm/keybindings/' > gnome3-keybind-backup/keybindings.dconf
-dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' > gnome3-keybind-backup/custom-values.dconf
-dconf read '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings' > gnome3-keybind-backup/custom-keys.dconf
+mkdir -p ./keybind-backup
+
+dconf dump '/org/gnome/desktop/wm/keybindings/' > ./keybind-backup/keybindings.dconf
+dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' > ./keybind-backup/custom-values.dconf
+dconf read '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings' > ./keybind-backup/custom-keys.dconf
 echo "backup done"
-exit 0
